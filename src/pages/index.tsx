ab0609400547f7b6cb274050416b65f74e2e71ed
Joao Ladeira <joaoladeirag@gmail.com>
2021-03-22 18:15:27 -0300

    finish home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        salve <span>salve</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br/> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications 
+            <span>for $9.90 monthly</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
    </>
   )
 }
