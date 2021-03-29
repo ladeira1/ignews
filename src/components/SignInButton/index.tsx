c332a8b1ac727a9ad239ff3dfaf5c83841d83da2
Joao Ladeira <joaoladeirag@gmail.com>
2021-03-28 22:37:00 -0300

    add authentication on posts

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -15,7 +15,7 @@ export function SignInButton() {
         onClick={() => signOut()}
       >
         <FaGithub color="#04d361" />
-        Joao Ladeira
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     )
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import Providers from 'next-auth/providers'
 import { query as q } from 'faunadb'
 
 import { fauna } from '../../../services/fauna'
+import { session } from 'next-auth/client'
 
 export default NextAuth({
   providers: [
@@ -14,6 +15,36 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
+              )
+            ])
+          )
+        )
+  
+        return { ...session, activeSubscription: userActiveSubscription }
+      } catch {
+        return { ...session, activeSubscription: null }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user
 
